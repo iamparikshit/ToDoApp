ca2a1bbcd78204d5085a658cdc2c924bf7c25816
Parikshit <jparikshit@gmail.com>
2020-05-12 02:46:13 +0530

    Updates

---
+++ precache-manifest.a305783f84974f412f6aede3ef1bb708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea536825d111500d9e09ecad36d895a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56302fc289069b91d302",
+    "url": "/static/css/main.87036e2d.chunk.css"
+  },
+  {
+    "revision": "3c08e2cfb0185c1a95f1",
+    "url": "/static/js/2.55214f1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55214f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56302fc289069b91d302",
+    "url": "/static/js/main.a9d53bb2.chunk.js"
+  },
+  {
+    "revision": "6c8f285986013edb66e9",
+    "url": "/static/js/runtime-main.5d95d180.js"
+  }
+]);
